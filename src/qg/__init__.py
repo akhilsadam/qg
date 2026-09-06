552b6bdead775c53871b91e7efce3d475dbbbe39
Akhil Sadam <sadam.akhil@gmail.com>
2026-09-06 13:39:49 -0400

    update install

--- src/qg/__init__.py
+++ src/qg/__init__.py
@@ -38,10 +38,8 @@ def solver(config_overrides: dict = None) -> QG:
     # Register configs
     register_configs()
     
-    # Get absolute path to config directory
+    # Get relative path to config directory
     config_dir = Path(__file__).parent / "conf"
-    # make it relative
-    config_dir = config_dir.relative_to(Path.cwd(), walk_up=True)
 
     # Initialize Hydra with config path
     with initialize(version_base="1.3", config_path=str(config_dir)):
